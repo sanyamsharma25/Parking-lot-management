8cb89aca0f56ebac29ae889318e07f6ec5e836a6
sanyamsharma25 <sanyamvats25@gmail.com>
2023-07-27 20:32:05 +0530

    initial commit

---
+++ validate.php
@@ -0,0 +1,20 @@
+<?php   
+session_start();
+$name = $_POST["username"];
+$pass = $_POST["password"];
+$conn = mysqli_connect("localhost", "root", "","login_info") or die("Connectiion failed!");
+$sql = "SELECT * FROM admin_info where username = '{$name}' && password = '{$pass}'";
+$result = mysqli_query($conn, $sql) or die("query failed!");
+$num = mysqli_num_rows($result);
+if ($num == 1){
+    $_SESSION["username"] = $name;
+    header("location:http://localhost/project1/main.php");
+    exit();
+}else{
+    
+    header("location:http://localhost/project1/index.php");
+    exit();
+}
+?>
+
+
